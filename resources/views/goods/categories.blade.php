e9f513f27e3b7bc75724c37974a67e01a00bdb05
Javlon <fedot.pushkin98@gmail.com>
2022-10-20 18:40:21 +0500

    add: adding categories & success pages

---
+++ resources/views/goods/categories.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.header')
+@section('content')
+    <div class="container">
+        <div class="card bg-dark text-white">
+            <img src="{{ asset('assets/images/product/1.png') }}" class="card-img" alt="...">
+            <div class="card-img-overlay">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text">Last updated 3 mins ago</p>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
